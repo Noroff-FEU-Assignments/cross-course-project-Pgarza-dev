9e4639b2cb84f35965a9df6b1cde027d817cdd9b
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-03-08 12:28:57 +0100

    event listener to add btns cart page

---
+++ js/cart.js
@@ -0,0 +1,36 @@
+const sizeButtons = document.querySelectorAll(".sizeButton");
+const addButtons = document.querySelectorAll(".insuranceButton");
+
+//changes bg color of size buttons
+function changeBackgroundColor() {
+  document.querySelector(".sizeButton").style.backgroundColor = "#ffaa00";
+  // document.querySelector("addButtons").style.backgroundColor = "#ffaa00";
+}
+sizeButtons.onclick = changeBackgroundColor;
+// addButtons.onclick = changeBackgroundColor;
+
+sizeButtons.forEach((sizeButton) => {
+  sizeButton.classList.remove("selected-size-btn");
+  sizeButton.addEventListener("click", () => {
+    sizeButtons.forEach((sizeButton) => {
+      sizeButton.classList.remove("selected-size-btn");
+    });
+    sizeButton.classList.add("selected-size-btn");
+  });
+});
+
+//changes bg color of add buttons
+function addButtonsBackgroundColor() {
+  document.querySelector(".insuranceButton").style.backgroundColor = "#ffaa00";
+}
+addButtons.onclick = addButtonsBackgroundColor;
+
+addButtons.forEach((addButton) => {
+  addButton.classList.remove("insuranceButton");
+  addButton.addEventListener("click", () => {
+    addButtons.forEach((addButton) => {
+      addButton.classList.remove("insuranceButton");
+    });
+    addButton.classList.add("insuranceButton");
+  });
+});
--- js/jackets.js
+++ js/jackets.js
@@ -1,15 +1,8 @@
 const sizeButtons = document.querySelectorAll(".sizeButton");
 
-// function clicked() {
-//   for (let i = 0; i < clickedButton.length; i++) {}
-//   clickedButton.style.backgroundColor = "green";
-//   // document.querySelector("button").style.backgroundColor = "red";
-// }
-
 function changeBackgroundColor() {
   document.querySelector(".sizeButton").style.backgroundColor = "#ffaa00";
 }
-console.log(sizeButtons);
 sizeButtons.onclick = changeBackgroundColor;
 
 sizeButtons.forEach((sizeButton) => {
@@ -21,7 +14,3 @@ sizeButtons.forEach((sizeButton) => {
     sizeButton.classList.add("selected-size-btn");
   });
 });
-// function secondClick() {
-//   document.querySelector(".button").style.backgroundColor = "#ffaa00";
-// }
-// clickedButton.onclick = secondClick;
